25052383304071b4ea7c1ad94fd76259a3015ceb
Vladyslav Rybak <vladyslavry141@gmail.com>
2024-05-17 14:14:05 +0300

    CA-2229 fix github pr checks

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,24 @@ import * as github from "@actions/github";
 import * as userway from "@userway/cicd-core";
 import { stripEmptyProperties } from "src/stripEmptyProperties";
 
-async function scan({
-  project = github.context.payload.repository?.name!,
-  commitHash = github.context.payload.pull_request?.head.sha ||
-    github.context.sha,
-  branch = github.context.payload.pull_request?.head.ref || github.context.ref,
-  target = github.context.payload.pull_request?.base.ref,
-  pullRequest = github.context.payload.pull_request?.number,
-  contributorName = github.context.actor,
-  ...config
-}: userway.OutputOptions) {
-  return await userway.scan(
-    {
-      project,
-      commitHash,
-      branch,
-      target,
-      pullRequest,
-      contributorName,
-      ...config,
-    },
-    { logger: { ...core, warn: core.warning } }
-  );
+function buildLogger() {
+  return { ...core, warn: core.warning };
 }
 
-async function run() {
-  const trimmed = stripEmptyProperties({
+function buildContextConfig() {
+  return userway.purgeUndefined({
+    project: github.context.payload.repository?.name!,
+    commitHash:
+      github.context.payload.pull_request?.head.sha ?? github.context.sha,
+    branch: github.context.payload.pull_request?.head.ref || github.context.ref,
+    target: github.context.payload.pull_request?.base.ref,
+    pullRequest: github.context.payload.pull_request?.number,
+    contributorName: github.context.actor,
+  });
+}
+
+function buildActionConfig() {
+  return stripEmptyProperties({
     config: core.getInput("config"),
 
     token: core.getInput("token"),
@@ -55,20 +47,32 @@ async function run() {
     dryRun: core.getInput("dry_run") === "true",
     verbose: core.isDebug(),
   });
+}
 
-  const file = await userway.read<object>(trimmed.config).catch(() => ({}));
+async function run() {
+  const actionConfig = buildActionConfig();
+  const contextConfig = buildContextConfig();
+
+  const fileConfig = await userway
+    .read<object>(actionConfig.config)
+    .catch(() => ({}));
+
+  core.debug(JSON.stringify({ actionConfig, contextConfig, file: fileConfig }));
 
   const config = await userway.config.parseAsync({
-    ...file,
-    ...trimmed,
+    ...contextConfig,
+    ...fileConfig,
+    ...actionConfig,
   });
+
+  core.debug(JSON.stringify({ config }));
 
   if (config.dryRun) {
     core.info(JSON.stringify(config));
     process.exit(0);
   }
 
-  return await scan(config);
+  return await userway.scan(config, { logger: buildLogger() });
 }
 
 run()
